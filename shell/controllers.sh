eca5e0d71a0e9d657e1cfd256ea2b2c357be7b60
vincenzodivita <divitavincenzo@icloud.com>
2023-07-14 15:54:44 +0200

    Create laravarel project with breeze

---
+++ shell/controllers.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Make new PageController inside Guest folder
+php artisan make:controller Guest/PageController
+
+# Make new PageController inside Admin folder
+php artisan make:controller Admin/PageController
